6fbc63d51e3f9caa5f5ed2115b536564ec155504
V Prentice <vcoprentice@gmail.com>
2021-10-14 16:34:11 -0700

    Code SuperComputerPlayer class to choose winning moves

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,20 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    current_node = TicTacToeNode.new(game.board, mark)
+    current_node.children.each do |child_node|
+      if child_node.winning_node?(mark)
+        return child_node.prev_move_pos
+      end
+    end
+
+    current_node.children.each do |child_node|
+      if !child_node.losing_node?(mark)
+        return child_node.prev_move_pos
+      end
+    end 
+
+    raise "No possible winning moves to make"
   end
 end
 
